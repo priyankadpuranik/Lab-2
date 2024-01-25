9aaa1b44e08e2d4697746b7f014c3b7eecb1ac4a
priyanka <01fe22bca010@kletech.ac.in>
2024-01-25 13:44:37 +0530

    biggest number

---
+++ biggest.java
@@ -0,0 +1,17 @@
+public class biggest {
+
+    public static void main(String[] args) {
+
+        double n1 = -4.5, n2 = 3.9, n3 = 2.5;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the biggest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the biggest number.");
+
+        else
+            System.out.println(n3 + " is the biggest number.");
+    }
+}
+  
